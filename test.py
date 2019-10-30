796ac4bca54080d8e073914140604f5ca41a9b1d
skytopsama <31357636+skytopsama@users.noreply.github.com>
2019-10-30 07:34:16 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+four = Fighter("4.0")
+four.set_base_attrs({"con":10, "int":10, "str":8, "agi":11, "dex":12})
+
+tula = Fighter("Tula")
+tula.set_base_attrs({"con":11, "int":10, "str":14, "agi":9, "dex":10})
+
+Blitz = Move('Blitz')
+Blitz.setup_move(move_type = "Melee", hit_chance = 0.8, move_cost = {"sp":10}, move_changes = {'hp':-15})
+four.moves = [Blitz]
